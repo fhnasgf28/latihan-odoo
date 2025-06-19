ff5a6da2843ae9aa8d8abb84e029e9a1e91dd928
fhnasgf28 <farhan.assegaf19019@student.unsika.ac.id>
2025-06-19 23:26:26 +0700

    [add] Total packaging details in invoice based on delivery

--- custom_module_nibble/models/account_move.py
+++ custom_module_nibble/models/account_move.py
@@ -1,6 +1,43 @@
-from odoo import models, fields
+from odoo import models, fields, api
 
 class AccountMove(models.Model):
     _inherit = 'account.move'
 
     nsfp = fields.Char(string="Nomor Seri Faktur Pajak (NSFP)")
+    total_packaging = fields.Float(string="Total Packaging", compute="_compute_total_packaging", store=True)
+    
+    @api.depends('invoice_line_ids.packaging_qty')
+    def _compute_total_packaging(self):
+        for move in self:
+            move.total_packaging = sum(move.invoice_line_ids.mapped('packaging_qty'))
+            
+class AccountMoveLine(models.Model):
+    _inherit = 'account.move.line'
+    
+    packaging_qty = fields.Float(string="Packaging Quantity")
+    
+    @api.model_create_multi
+    def create(self, vals_list):
+        """Override to set packaging_qty from sale_line_ids if available."""
+        lines = super().create(vals_list)
+        
+        for line in lines:
+            # If this invoice line is linked to sale order lines
+            sale_lines = line.sale_line_ids
+            if sale_lines:
+                packaging_qty = 0
+                for sale_line in sale_lines:
+                    # Get stock moves related to this sale line that are done
+                    moves = sale_line.move_ids.filtered(lambda m: m.state == 'done')
+                    for move in moves:
+                        if move.product_packaging_id:
+                            packaging_qty += move.product_packaging_quantity
+                        else:
+                            # If no specific packaging is set but there's a quantity
+                            if move.quantity > 0:
+                                packaging_qty += move.quantity
+                
+                if packaging_qty > 0:
+                    line.packaging_qty = packaging_qty
+        
+        return lines
