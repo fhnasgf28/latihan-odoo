4569ffca5ac8b23fe1e1968558f4cc04f7cffd4b
fhnasgf28 <assegaffarhan5@gmail.com>
2026-04-16 10:06:07 +0700

    menambahkan priority

--- custom_module_nibble/models/purchase_request.py
+++ custom_module_nibble/models/purchase_request.py
@@ -4,3 +4,8 @@ class PurchaseRequestLine(models.Model):
     _inherit = 'purchase.request.line'
 
     notes = fields.Text(string="Notes")
+    priority = fields.Selection([
+        ('low', 'Low'),
+        ('medium', 'Medium'),
+        ('high', 'High'),
+    ], string="Priority", default='medium')
